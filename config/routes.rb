c6348f8b57cff068de3ee766c80111b2bc8f613b
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-07 00:01:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  # resources :authors, only: [:show]
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-  root 'posts#index'
+  # root 'posts#index'
 
 end
