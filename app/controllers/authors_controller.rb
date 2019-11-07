34fdc357bba6ecbfebfe953966694de4663761cf
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-07 00:18:30 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,8 @@ class AuthorsController < ApplicationController
   
   def post 
     @author = Author.find(params[:id])
+    @post = Post.find(params[:post_id])
+    render template: 'posts/show'
   end 
 
 end
